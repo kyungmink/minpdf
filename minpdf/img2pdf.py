e277b7cb2cb92de480fe18405ec9672fbfdde519
Kyungmin Ko <funfrocks@gmail.com>
2023-10-04 12:10:56 -0500

    Added comments to img2pdf.

--- minpdf/img2pdf.py
+++ minpdf/img2pdf.py
@@ -46,12 +46,15 @@ def to_realsize_pdf(imgfile, dpi:int=150, save_bytes_factor:int=1):
     '''Converts to letter-sized pdf pages.
     
     :returns: new pdf filename.'''
+    # to_letter_ratio returns a file path string.
     newFile = to_letter_ratio(imgfile, dpi=dpi, save_bytes_factor=save_bytes_factor)
     dpi = dpi / save_bytes_factor
     pdfdata = img2pdf.convert([newFile])
     pdf = Pdf.open(BytesIO(pdfdata))
     remove(newFile)
     page = pdf.pages[0]
+    # Define page. The PDF default resolution is 72 points per inch, and the
+    # U.S. letter size (8.5 x 11 in.) is 612 x 792 points.
     page.mediabox = [0, 0, int(612), int(792)]
     from pikepdf import parse_content_stream, PdfMatrix, PdfImage, Array, unparse_content_stream
     rawimage = page.images['/Im0']  # The raw object/dictionary
@@ -59,11 +62,40 @@ def to_realsize_pdf(imgfile, dpi:int=150, save_bytes_factor:int=1):
     commands = [[operands, operator] for operands, operator 
               in parse_content_stream(page)]
     original = PdfMatrix(commands[1][0])
+    # Calculate the transformation required to center the image on the page
+    # and scale it according to specified original physical resolution.
+    # Then apply the transformation by postmultiplication (i.e., after)
+    # the transformation already applied by img2pdf.
+    # img2pdf applies a transformation that treats the image like a 96
+    # pixel-per-inch image. Specifically, it maps 96 pixels to 72 points in
+    # the PDF. The use of the term "points" is confusing because it is a
+    # measure of distance and not a measure of information in the PDF space (
+    # one point has 4/3 pixels mapped onto it). Henceforth I will use the term
+    # "unit" to refer to a "point" in the default PDF space to avoid this
+    # confusion. img2pdf maps 96 pixels to a distance of 72 units. To center
+    # an image onto a physical medium (in this case the U.S. letter paper), it
+    # it can be translated either before or after scaling. It is simple to
+    # center it in its native resolution (pixels per inch). If the native
+    # resolution is r and its pixel dimension is w then to center it in a
+    # space of w' it must be translated by (rw' - w)/2 pixels. Any subsequent
+    # scaling preserves the origin (0, 0) of the axis/axes, which is the
+    # corner of the medium. After the scaling operation by img2pdf (by a
+    # factor of 3/4), the equivalent translation should be also scaled.
+    # Therefore it should be (rw' - w) / 2 * (3/4). Substituting 8.5 (in.) for
+    # w' and refactoring it to keep computations in the numerator in the
+    # integer group/space, gives (51r - 6w) / 16 for the horizontal axis.
+    # Subsequent scaling to map r pixels in the original image to 72 units
+    # must squish r pixels into the 72-unit space into which img2pdf had
+    # squished 96 pixels. The scaling factor therefore equals 96/r.
+    #
+    # Apply the translation and scaling transformations using the solution
+    # above. Compute the resulting CTM.
     new_matrix     = original    .translated(
           (51 * dpi - pdfimage.width * 6) / 16
         , (66 * dpi - pdfimage.height * 6) / 16
     )\
     .scaled(96 / dpi, 96 / dpi)
+    # Substitute the CTM into the PDF.
     commands[1][0] = Array([*new_matrix.shorthand])
     new_content_stream = unparse_content_stream(commands)
     page.Contents = pdf.make_stream(new_content_stream)
